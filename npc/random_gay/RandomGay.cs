88c7dba1ea07d4b3ecb4bb44ef0206d446685b80
TheStilk <serkebaevmadiyar08@gmail.com>
2025-11-11 10:45:06 +0500

    rand gay

--- npc/random_gay/RandomGay.cs
+++ npc/random_gay/RandomGay.cs
@@ -1,158 +1,41 @@
-using Godot;
+extends "res://Scripts/Characters/Animation/Skeleton.gd"
 
-public partial class RandomGay : CharacterBody3D
-{
-	[Export]
-	public float Speed = 1.5f;
 
-	// Переменная для гравитации
-	private float _gravity = (float)ProjectSettings.GetSetting("physics/3d/default_gravity");
+@export var min_max_interpolation: Vector2 = Vector2(0.0, 0.9)
+@export var foot_height_offset: float = 0.2
 
-	// Ноды
-	private Godot.AnimationPlayer _animationPlayer;
-	private Godot.NavigationAgent3D _navAgent;
-	private Godot.Timer _timer;
+#IK variables
+@onready var target_left = $Target_left #targets for the feet
+@onready var target_right = $Target_right
+@onready var raycast_left = $RayCast_left #raycasts that detect weather player is on the floor
+@onready var raycast_right  = $RayCast_right
+@onready var ik_left = $Armature001/Skeleton3D/SkeletonIK_left_foot #gets the skeletonIK bones
+@onready var ik_right = $Armature001/Skeleton3D/SkeletonIK_right_foot
+@onready var no_raycast_pos_l = $no_raycast_pos_l #gets the 
+@onready var no_raycast_pos_r = $no_raycast_pos_r
 
-	// Состояния
-	private enum State { Idle, Walking }
-	private State _state = State.Idle;
+#physical_skeleton
+@onready var skeleton = $Armature001/Skeleton3D
 
-	[Export]
-	private int _walkingDuration = 5;
-	[Export]
-	private int _idleDuration = 5;
-
-	public override void _Ready()
-	{
-		_animationPlayer = GetNode<Godot.AnimationPlayer>("AnimationPlayer");
-		_navAgent = GetNode<Godot.NavigationAgent3D>("NavigationAgent3D");
-		_timer = GetNode<Godot.Timer>("Timer");
-
-		_timer.Timeout += OnTimerTimeout;
-		_timer.Start(_idleDuration);
-		
-		_animationPlayer.Play("Idle", 0.3f); 
-	}
-
-	public override void _Process(double delta)
-	{
-		// Проверка на null
-		if (_navAgent == null || _animationPlayer == null) return;
-		
-		// --- ДОБАВЛЕНА ГРАВИТАЦИЯ ---
-		// Получаем текущую Velocity
-		Vector3 velocity = Velocity;
-
-		// Применяем гравитацию, если не на полу
-		if (!IsOnFloor())
-		{
-			velocity.Y -= _gravity * (float)delta;
-		}
-
-		switch (_state)
-		{
-			case State.Walking:
-				if (_navAgent.IsNavigationFinished())
-				{
-					// Дошли до цели
-					_state = State.Idle;
-					_timer.Start(_idleDuration);
-					_animationPlayer.Play("Idle", 0.3f);
-					
-					// Обнуляем горизонтальную скорость
-					velocity.X = 0;
-					velocity.Z = 0;
-				}
-				else
-				{
-					// Еще идем, обновляем скорость из MoveTowardTarget
-					velocity = MoveTowardTarget(Speed, velocity);
-				}
-				break;
-
-			case State.Idle:
-				_animationPlayer.Play("Idle", 0.3f);
-				
-				// Обнуляем горизонтальную скорость
-				velocity.X = 0;
-				velocity.Z = 0;
-				break;
-		}
-		
-		// Применяем итоговую скорость (с гравитацией)
-		Velocity = velocity;
-		MoveAndSlide();
-	}
+func _ready():
+	#start IK
+	ik_right.start()
+	ik_left.start()
 	
-	// --- ФУНКЦИЯ ИЗМЕНЕНА ---
-	private Vector3 MoveTowardTarget(float moveSpeed, Vector3 currentVelocity)
-	{
-		Vector3 nextPosition = _navAgent.GetNextPathPosition();
-		
-		// 1. ИСПРАВЛЕНИЕ ДВИЖЕНИЯ:
-		// Считаем направление движения (только по XZ)
-		Vector3 moveDirection = (nextPosition - GlobalTransform.Origin).Normalized();
-		
-		// Сохраняем вертикальную скорость (гравитацию)
-		float verticalSpeed = currentVelocity.Y;
-		
-		// Задаем новую горизонтальную скорость
-		Vector3 newVelocity = new Vector3(
-			moveDirection.X * moveSpeed,
-			verticalSpeed,
-			moveDirection.Z * moveSpeed
-		);
+	#skeleton.physical_bones_start_simulation() #make sure the shoulder physical bones are always set to the translatiopn of the shoulder bones
 
-		// 2. ИСПРАВЛЕНИЕ ПОВОРОТА:
-		// Проверяем, что есть куда двигаться
-		if (moveDirection.Length() > 0)
-		{
-			// --- ЭТО ГЛАВНОЕ ИСПРАВЛЕНИЕ ДЛЯ ПОВОРОТА ---
-			// Мы смотрим не НА цель, а В ПРОТИВОПОЛОЖНУЮ сторону
-			// Это "обманывает" LookAt, чтобы он поворачивал модель (которая смотрит на +Z) правильно
-			Vector3 lookTarget = GlobalTransform.Origin - moveDirection;
-			
-			Basis targetRotation = GlobalTransform.LookingAt(lookTarget, Vector3.Up).Basis;
-			
-			Transform3D newTransform = GlobalTransform;
-			// Используем Orthonormalized() для избежания ошибок с масштабированием
-			newTransform.Basis = newTransform.Basis.Orthonormalized().Slerp(targetRotation, 0.1f);
-			GlobalTransform = newTransform;
-		}
+func update_ik_target_pos(target, raycast, no_raycast_pos, foot_heighy_offset):
+	if raycast.is_colliding(): #if raycast is on ground
+		var hit_point = raycast.get_collision_point().y + foot_height_offset #gets Y position of where the ground is.
+		target.global_transform.origin.y = hit_point #sets the target to the y position of the hitpoint
+	else:
+		target.global_transform.origin.y = no_raycast_pos.global_transform.origin.y #if the raycast not colliding, the player is in the air and so the target position is set to the no_raycast_pos
 
-		// 3. АНИМАЦИЯ
-		if (newVelocity.LengthSquared() > 0.1f) // LengthSquared() быстрее, чем Length()
-		{
-			_animationPlayer.Play("Walk", 0.3f);
-		}
-
-		return newVelocity;
-	}
-
-	private void OnTimerTimeout()
-	{
-		if (_navAgent == null || _animationPlayer == null) return;
-
-		switch (_state)
-		{
-			case State.Idle:
-				var randomTarget = new Vector3(
-					(float)GD.RandRange(-10, 10), 
-					0, 
-					(float)GD.RandRange(-10, 10)
-				);
-				
-				_navAgent.TargetPosition = randomTarget;
-				_state = State.Walking;
-				
-				_timer.Start(_walkingDuration); 
-				break;
-			
-			case State.Walking:
-				_state = State.Idle;
-				_timer.Start(_idleDuration);
-				_animationPlayer.Play("Idle", 0.3f);
-				break;
-		}
-	}
-}
+func _physics_process(_delta):
+	#update target_pos each frame
+	update_ik_target_pos(target_left, raycast_left, no_raycast_pos_l, foot_height_offset)
+	update_ik_target_pos(target_right, raycast_right, no_raycast_pos_r, foot_height_offset)
+	
+	#update interpolation each frame
+	ik_right.interpolation = clamp($Interpolation_right.transform.origin.y, min_max_interpolation.x, min_max_interpolation.y)
+	ik_left.interpolation = clamp($Interpolation_left.transform.origin.y, min_max_interpolation.x, min_max_interpolation.y)
